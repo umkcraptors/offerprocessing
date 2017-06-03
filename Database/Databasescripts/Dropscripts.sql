b6030f3f4200e5c300b31a1c961c6f90c50332a6
Rama Krishna <ramakrishna.candy@gmail.com>
2017-06-03 13:24:13 -0400

    .sql files added

---
+++ Database/Databasescripts/Dropscripts.sql
@@ -0,0 +1,29 @@
+DROP table SupplierProduct;
+
+DROP table SupplierOffer;
+
+DROP table AddressType;
+
+DROP table Status;
+
+DROP table OfferMatch;
+
+DROP table Product;
+
+DROP table Supplier;
+
+DROP table Address;
+
+DROP table ConsumerOffer;
+
+DROP table MarketerProduct;
+
+DROP table MarketerPreferredSupplier;
+
+DROP table Marketer;
+
+DROP table Consumers;
+
+
+
+
